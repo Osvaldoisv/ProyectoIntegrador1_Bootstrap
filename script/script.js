b17d80ae19957562858774470d58d94bc98e8457
Tomás Aravena <taravenab@uft.edu>
2024-02-14 17:41:47 -0300

    cambios popup

--- script/script.js
+++ script/script.js
@@ -1,1 +1,17 @@
-
+// script.js
+document.addEventListener('DOMContentLoaded', function () {
+    const openPopupBtn = document.getElementById('openPopupBtn');
+    const popupForm = document.getElementById('popupForm');
+  
+    openPopupBtn.addEventListener('click', function () {
+      popupForm.style.display = 'block';
+    });
+  
+    // Agregar lógica para cerrar el popup cuando se envía el formulario
+    const form = popupForm.querySelector('form');
+    form.addEventListener('submit', function (e) {
+      e.preventDefault(); // Evitar que se envíe el formulario (puedes manejar esto según tus necesidades)
+      popupForm.style.display = 'none';
+    });
+  });
+  
