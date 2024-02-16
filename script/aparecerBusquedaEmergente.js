98ba927680c763b07e2146a23a228bad9e91c402
Osvaldo Sepulveda <osvaldo.sepulveda.v@ug.uchile.cl>
2024-02-16 01:16:45 -0300

    mejorar y traducir buscador

--- script/aparecerBusquedaEmergente.js
+++ script/aparecerBusquedaEmergente.js
@@ -2,8 +2,8 @@ document.getElementById('mostrarBtn').addEventListener('click', function() {
     var container = document.getElementById('miContainer');
     container.classList.toggle('mostrando'); // Alternar clase para mostrar/ocultar
     if (container.classList.contains('mostrando')) {
-        this.textContent = 'Ocultar Container';
+        this.textContent = 'BÚSQUEDA EMERGENTE';
     } else {
-        this.textContent = 'Mostrar Container';
+        this.textContent = 'BÚSQUEDA EMERGENTE';
     }
 });
